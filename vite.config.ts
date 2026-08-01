1b5bebecb98dc977bf1e2379526c038580046669
Ruth Dos Anjos <ruthinhaemilly@hotmail.com>
2026-08-01 19:30:38 -0300

    fix: correct vite server watch configuration

--- vite.config.ts
+++ vite.config.ts
@@ -33,14 +33,9 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       port: parseInt(process.env.PORT || '8443'),
       strictPort: true,
-     server: {
-      host: '0.0.0.0',
-      port: parseInt(process.env.PORT || '8443'),
-      strictPort: true,
-      watch: { 
-        ignored: ['**/.figma/**', '**/tsconfig.json', '**/tsconfig.*.json'] 
+      watch: {
+        ignored: ['**/.figma/**', '**/tsconfig.json', '**/tsconfig.*.json'],
       },
-    },
     },
     preview: {
       host: '0.0.0.0',
